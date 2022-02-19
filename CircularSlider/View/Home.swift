eaf8879df15da41199ef2c7a5199893b1d233835
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-02-19 18:47:58 +0300

    Start project

---
+++ CircularSlider/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  CircularSlider
+//
+//  Created by Danil Peregorodiev on 19.02.2022.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
