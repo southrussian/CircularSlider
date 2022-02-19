050e783160f47ac0be95414217cbea79a68ab40a
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-02-19 18:40:50 +0300

    Initial Commit

---
+++ CircularSlider/CircularSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSliderApp.swift
+//  CircularSlider
+//
+//  Created by Danil Peregorodiev on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CircularSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircularSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CircularSlider
+//
+//  Created by Danil Peregorodiev on 19.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
