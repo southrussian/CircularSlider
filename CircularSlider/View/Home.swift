9d2a9ce2c7c4ea1b8d4631cf040e6ab7b9961952
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-02-19 19:13:42 +0300

    Added personal welcoming

--- CircularSlider/View/Home.swift
+++ CircularSlider/View/Home.swift
@@ -9,12 +9,47 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            HStack {
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    
+                    let date = Date().dayOfWeek()!
+                    Text("\(date)")
+                        .font(.title.bold())
+                    Text("Доброе утро, Шлёпа")
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Button {
+                    
+                } label: {
+                    Image("floppa")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 45, height: 45)
+                        .clipShape(Circle())
+                    
+                }
+            }
+        }
+        .padding()
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+    }
+}
+
+extension Date {
+    func dayOfWeek() -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: self).capitalized
+        // or use capitalized(with: locale) if you want
     }
 }
